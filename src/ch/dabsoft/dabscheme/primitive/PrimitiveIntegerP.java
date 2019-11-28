1bbb6644acaf0ed367317746a40977d42686d969
Damian Brunold <damian.brunold@bluewin.ch>
2019-11-28 11:42:47 +0100

    Fix integer? to detect inexact integers

--- src/ch/dabsoft/dabscheme/primitive/PrimitiveIntegerP.java
+++ src/ch/dabsoft/dabscheme/primitive/PrimitiveIntegerP.java
@@ -8,7 +8,12 @@ public class PrimitiveIntegerP extends Primitive {
     @Override
     public Object apply(Object[] arguments) {
         if (arguments.length != 1) throw new IllegalStateException("integer? expected 1 argument, but got " + arguments.length);
-        return Value.isInteger(arguments[0]);
+        if (Value.isInteger(arguments[0])) return Value.T;
+        if (Value.isReal(arguments[0])) {
+            double val = toReal(arguments[0]);
+            return val == (double) ((long) val) ? Value.T : Value.F;
+        }
+        return Value.F;
     }
 
     @Override
