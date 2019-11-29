6ef9586ad6074151722f8baef89161711efb0024
Damian Brunold <damian.brunold@bluewin.ch>
2019-11-29 22:16:31 +0100

    Add calculator example from tspl3ed

--- src.test/tests_tspl3ed.scm
+++ src.test/tests_tspl3ed.scm
@@ -1516,3 +1516,55 @@ d
 =>
 #t
 <<
+
+(define calc #f)
+(let ()
+  (define do-calc
+    (lambda (ek exp)
+      (cond
+       ((number? exp) exp)
+       ((and (list? exp) (= (length exp) 3))
+	(let ((op (car exp)) (args (cdr exp)))
+	  (case op
+	    ((add) (apply-op ek + args))
+	    ((sub) (apply-op ek - args))
+	    ((mul) (apply-op ek * args))
+	    ((div) (apply-op ek / args))
+	    (else (complain ek "invalid operator" op)))))
+       (else (complain ek "invalid expression" exp)))))
+  (define apply-op
+    (lambda (ek op args)
+      (op (do-calc ek (car args)) (do-calc ek (cadr args)))))
+  (define complain
+    (lambda (ek msg exp)
+      (ek (list msg exp))))
+  (set! calc
+	(lambda (exp)
+	  (call/cc
+	   (lambda (ek)
+	     (do-calc ek exp))))))
+=
+(calc '2)
+=>
+2
+.
+(calc '(add 2 3))
+=>
+5
+.
+(calc '(add (mul 3 2) -4))
+=>
+2
+.
+(calc '(div 6 2))
+=>
+3
+.
+(calc '(add (mul 3 2) (div 4)))
+=>
+("invalid expression" (div 4))
+.
+(calc '(mul (add 1 -2) (pow 2 7)))
+=>
+("invalid operator" pow)
+<<
