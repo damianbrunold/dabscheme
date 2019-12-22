1b8b08799894b7f738c1d1a87b1d6d742e7f45ab
Damian Brunold <damian.brunold@bluewin.ch>
2019-12-22 21:20:06 +0100

    Fix arg count in dict-keys procedure

--- src/ch/dabsoft/dabscheme/primitive/PrimitiveDictKeys.java
+++ src/ch/dabsoft/dabscheme/primitive/PrimitiveDictKeys.java
@@ -13,7 +13,7 @@ public class PrimitiveDictKeys extends Primitive {
 
     @Override
     public Object apply(Object[] arguments) {
-        if (arguments.length != 2) throw new IllegalStateException("dict-keys expected 1 argument, but got " + arguments.length);
+        if (arguments.length != 1) throw new IllegalStateException("dict-keys expected 1 argument, but got " + arguments.length);
         HashMap dict = Value.asDict(arguments[0]);
         List<char[]> keys = new ArrayList<>();
         for (Object key : dict.keySet()) {
